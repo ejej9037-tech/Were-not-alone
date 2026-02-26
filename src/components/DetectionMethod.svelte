9cfec3fbde10ba94d6d1e87db8c6f4f405cb3634
ejej9037-tech <ejej9037@gmail.com>
2026-02-25 20:14:24 -0500

    Create DetectionMethod.svelte component to display detection methods

---
+++ src/components/DetectionMethod.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let method;
+</script>
+
+<div class="detection-method">
+  <h2>{method.title}</h2>
+  <p>{method.description}</p>
+  <p><strong>Type:</strong> {method.type}</p>
+  <p><strong>Accuracy:</strong> {method.accuracy}%</p>
+</div>
