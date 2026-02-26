8e3a9714338d4834f7328dd1f94555de0b04b6cb
ejej9037-tech <ejej9037@gmail.com>
2026-02-25 19:55:37 -0500

    Add Vite configuration

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// Vite configuration file for Svelte project
+
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    plugins: [sveltekit()]
+});
