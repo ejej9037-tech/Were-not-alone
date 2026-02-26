9b8b52f286766fa02100879cc4a6c345b757c168
ejej9037-tech <ejej9037@gmail.com>
2026-02-25 20:00:45 -0500

    Create src/App.svelte

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+  import Header from './components/Header.svelte';
+  import Dashboard from './pages/Dashboard.svelte';
+  import Explorer from './pages/Explorer.svelte';
+  import About from './pages/About.svelte';
+
+  let currentPage = 'dashboard';
+
+  function navigateTo(page) {
+    currentPage = page;
+  }
+</script>
+
+<Header on:navigate={e => navigateTo(e.detail)} />
+
+<main>
+  {#if currentPage === 'dashboard'}
+    <Dashboard />
+  {:else if currentPage === 'explorer'}
+    <Explorer />
+  {:else if currentPage === 'about'}
+    <About />
+  {/if}
+</main>
+
+<style>
+  main {
+    padding: 2rem;
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+</style>
