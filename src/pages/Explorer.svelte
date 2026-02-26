fa264c622c75a23c4839e10bd0110d9770f2411f
ejej9037-tech <ejej9037@gmail.com>
2026-02-25 20:12:42 -0500

    Create Explorer.svelte for interactive detection methods explorer

---
+++ src/pages/Explorer.svelte
@@ -0,0 +1,19 @@
+<script>
+  let methods = [
+    { name: 'Method A', description: 'Description of Method A' },
+    { name: 'Method B', description: 'Description of Method B' },
+    { name: 'Method C', description: 'Description of Method C' },
+  ];
+</script>
+
+<main>
+  <h1>Interactive Detection Methods Explorer</h1>
+  <ul>
+    {#each methods as method}
+      <li>
+        <h2>{method.name}</h2>
+        <p>{method.description}</p>
+      </li>
+    {/each}
+  </ul>
+</main>
